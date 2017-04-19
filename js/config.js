c4d0615a31a56c01b508efb618daa9b4321049eb
Kawanlabs <if08017@gmail.com>
2017-04-19 21:26:44 +0700

    no message

---
+++ js/config.js
@@ -0,0 +1,18 @@
+/******************* MENUS *******************/
+var menu = ["HOME","ABOUT","PROMOTION","MENU","LOCATION","CONTACT"];
+
+/******************* HEADLIGHT *******************/
+var headlight = '{"title":[' +
+'{"background":"images/home-bg.jpg","headlight3":"Lorem Ipsum is simply dummy","headlight2":"Lorem Ipsum is simply dummy" },' +
+'{"background":"images/home-bg1.jpg","headlight3":"Lorem Ipsum is simply dummy","headlight2":"Lorem Ipsum is simply dummy" },' +
+'{"background":"images/home-bg2.jpg","headlight3":"Lorem Ipsum is simply dummy","headlight2":"Lorem Ipsum is simply dummy" }]}';
+
+
+/******************* ABOUT *******************/
+
+
+/******************* PROMOTION *******************/
+
+
+/******************* CONTACT *******************/
+
